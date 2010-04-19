2d871c5c4f7dc6575fbe7065e4bc5f2878627447
marcelgmr <marcelgmr@ea4d28a8-b21e-bf5b-143b-aa99eb0d973b>
2010-04-19 20:14:10 +0000

    License.

--- trunk/src/zeval.c
+++ trunk/src/zeval.c
@@ -1,4 +1,23 @@
 /*
+ * Copyright 2010 by Marcel Rodrigues <marcelgmr@gmail.com>
+ *
+ * This file is part of zap.
+ *
+ * zap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with zap.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * Evaluation
  */
 
