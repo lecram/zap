7149533dcfe5666195801592fc841c9e4644fe22
marcelgmr <marcelgmr@ea4d28a8-b21e-bf5b-143b-aa99eb0d973b>
2010-06-11 17:29:09 +0000

    Runtime functions moved from zap to zruntime.

--- src/include/zruntime.h
+++ src/include/zruntime.h
@@ -16,10 +16,43 @@
  * along with zap.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* Evaluation (header) */
+/* Runtime (header) */
 
+/* Bytecode Tokens */
+#define BLOCKEXIT   0xBE
+#define DELETE      0xDE
+#define BLOCK       0xB0
+#define IF          0x01
+#define ELIF        0x02
+#define ELSE        0x03
+#define WHILE       0x04
+#define DEF         0x05
+#define END         0x01
+#define BREAK       0x02
+#define CONTINUE    0x03
+#define RETURN      0x04
+
+/* Block Exit Flags */
+#define BE_END      0x80
+#define BE_BREAK    0x40
+#define BE_CONTINUE 0x20
+#define BE_RETURN   0x10
+
+typedef struct {
+    Dict *universal;
+    Dict *global;
+    Dict *local;
+} Space;
+
+Space *newspace();
+void delspace(Space **space);
 unsigned int readword(char **entry);
 void skip_expr(char **entry);
 Zob *eval(Dict *namespace, List *tmp, char **entry);
 Zob *nameval(Dict *namespace, char **entry);
 Zob *feval(Dict *namespace, List *tmp, char **entry);
+void skip_assign(char **entry);
+void assign(Dict *dict, Zob *value, char **entry);
+void runstatement(Space *space, List *tmp, char **entry);
+void skip_block(char **entry);
+unsigned char run_block(Space *space, List *tmp, char looplev, char **entry);
