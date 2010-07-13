7bf685ab2584a6413f4fd1b2e68044ab2e45f523
marcelgmr <marcelgmr@ea4d28a8-b21e-bf5b-143b-aa99eb0d973b>
2010-07-13 19:21:01 +0000

    Compiling scripts without del, assignments or blocks.

--- trunk/src/zcpl_mod.c
+++ trunk/src/zcpl_mod.c
@@ -22,44 +22,110 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <ztypes.h>
+#include "zcpl_expr.h"
 
-#include <zbyte.h>
-#include <zbytearray.h>
-#include <zlist.h>
+void
+hidequoted(char *str, char *quoted)
+{
+    char *s = str, *q = quoted;
+    int quoting = 0;
+    char qtype = '\0';
+
+    while (*s != '\0') {
+        if (quoting) {
+            if (*s == qtype)
+                quoting = 0;
+            else if (*s == '\\') {
+                *q = '\\';
+                *s = ' ';
+                s++;
+                q++;
+                *q = *s;
+                *s = ' ';
+                q++;
+            }
+            else {
+                *q = *s;
+                *s = ' ';
+                q++;
+            }
+        }
+        else if (*s == '"' || *s == '\'') {
+                quoting = 1;
+                qtype = *s;
+        }
+        s++;
+    }
+}
+
+void
+showquoted(char *str, char *quoted)
+{
+    char *s = str, *q = quoted;
+    int quoting = 0;
+    char qtype = '\0';
+
+    while (*s != '\0') {
+        if (quoting) {
+            if (*s == qtype)
+                quoting = 0;
+            else {
+                *s = *q;
+                q++;
+            }
+        }
+        else if (*s == '"' || *s == '\'') {
+                quoting = 1;
+                qtype = *s;
+        }
+        s++;
+    }
+}
 
 int
 cpl_mod(char *srcname)
 {
     FILE *fsrc, *fbin;
-    char *binname, line[256];
-    List *lines = newlist();
-    Node *lineyarr;
+    char *binname, *expr_entry;
+    char line[256], quoted[256], bin[256];
+    unsigned int length;
 
     fsrc = fopen(srcname, "r");
     if (fsrc == NULL) {
         printf("Error: Cannot open file \"%s\".\n", srcname);
         return 0;
     }
-    while (fgets(line, 256, fsrc) != NULL)
-        appitem(lines, (Zob *) yarrfromstr(line));
-    fclose(fsrc);
     binname = (char *) malloc(strlen(srcname) + 3);
     if (binname == NULL) {
         printf("Error: Out of memory.\n");
+        fclose(fsrc);
         return 0;
     }
     strcpy(binname, srcname);
     strcat(binname, "bc_");
     fbin = fopen(binname, "wb");
-    fclose(fbin);
-    printf("%u lines read from %s.\n\n", lines->length, srcname);
-    lineyarr = lines->first;
-    while (lineyarr != NULL) {
-        printf("%s", ((ByteArray *) lineyarr->object)->bytes);
-        lineyarr = lineyarr->next;
+    if (fbin == NULL) {
+        printf("Error: Cannot open file \"%s\".\n", binname);
+        fclose(fsrc);
+        free(binname);
+        binname = NULL;
+        return 0;
     }
-
+    while (fgets(line, 256, fsrc) != NULL) {
+        hidequoted(line, quoted);
+        if (strchr(line, ':') != NULL)
+            break;
+        showquoted(line, quoted);
+        expr_entry = line;
+        length = cpl_expr(&expr_entry, bin);
+        fwrite(bin, 1, length, fbin);
+        /* No Assignment. */
+        fwrite("\0", 1, 1, fbin);
+    }
+    fclose(fsrc);
+    /* Block End. */
+    fwrite("\xBE\x01", 1, 2, fbin);
+    fclose(fbin);
     free(binname);
     binname = NULL;
 
